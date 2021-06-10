517d9e9ced2ab9921536163841260e887982dd4c
EduardoACedillo <eduartcedillo@gmail.com>
2021-06-10 12:00:24 -0500

    Borre codigo comentado del index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-// const element = document.createElement('h1');
-// element.innerText = "Hello, Platzi Badges!";
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const jsx = <h1>Hello, Platzi Badges from React!</h1>;
 // const name = 'Richard';
 // const sum = () => 3 + 3;
 // const element = React.createElement('a',{ href:'https://platzi.com' },'Ir a platzi');
